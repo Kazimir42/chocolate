f7e7484626104ecd5c56644e9f57cd9bcecf59f8
Rémi Champlon <r.champlon@vold.lu>
2024-08-10 17:51:01 +0200

    base

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google';
+import {Inter} from 'next/font/google';
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata = {
     title: "PWA NextJS",
@@ -16,20 +16,21 @@ export const metadata = {
         },
     ],
     icons: [
-        { rel: "apple-touch-icon", url: "/icons/icon-192.png" },
-        { rel: "icon", url: "/icons/icon-192.png" },
+        {rel: "apple-touch-icon", url: "/icons/icon-192.png"},
+        {rel: "icon", url: "/icons/icon-192.png"},
     ],
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({children}) {
     return (
         <html lang="en">
         <head>
-            <meta name="theme-color" content="#111111" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="manifest" href="/manifest.json" />
-            <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192.png" />
-            <link rel="icon" href="/icons/icon-192.png" />
+            <meta name="theme-color" content="#111111"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link rel="manifest" href="/manifest.json"/>
+            <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192.png"/>
+            <link rel="icon" href="/icons/icon-192.png"/>
+            <title>{metadata.title}</title>
         </head>
         <body className={inter.className}>{children}</body>
         </html>
--- app/page.js
+++ app/page.js
@@ -1,113 +1,181 @@
-import Image from "next/image";
+'use client'
+import {useEffect, useState, useRef} from "react";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [isLoading, setIsLoading] = useState(true);
+    const [currentStep, setCurrentStep] = useState(null);
+    const [nextStep, setNextStep] = useState(null);
+    const [currentCycle, setCurrentCycle] = useState(1);
+    const [currentRound, setCurrentRound] = useState(1);
+    const [isCounting, setIsCounting] = useState(false);
+    const [timer, setTimer] = useState(0);
+    const intervalRef = useRef(null); // Utilisation de useRef pour conserver l'intervalle
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    useEffect(() => {
+        defineCurrentAndNextStep()
+    }, []);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    useEffect(() => {
+        // Nettoyage lorsque le composant est démonté
+        return () => clearInterval(intervalRef.current);
+    }, []);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    useEffect(() => {
+        if (currentStep) {
+            setIsLoading(false)
+        }
+    }, [currentStep])
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    useEffect(() => {
+        // The round is finish
+        if (timer === currentStep?.duration) {
+            finishRound()
+        }
+    }, [timer])
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    const steps = [
+        {
+            id: 1, name: 'Repos 1', duration: 5, in_progress: true,
+        },
+        {
+            id: 2, name: 'Repos 2', duration: 3, in_progress: false,
+        },
+        /*
+        {
+            id: 3, name: 'Repos 3', duration: 4, in_progress: false,
+        }
+        */
+    ]
+
+    const cycles_number = '5';
+
+    function defineCurrentAndNextStep() {
+        let found = false;
+        setNextStep(null)
+
+        // Find in progress step
+        steps.forEach((step, key) => {
+            if (step.in_progress) {
+                found = true;
+                setCurrentStep(step);
+                if (steps[key + 1]) {
+                    setNextStep(steps[key + 1])
+                }
+            }
+        })
+
+        // No step in progress, set the first so
+        if (!found) {
+            steps[0].in_progress = true;
+            setCurrentStep(steps[0])
+            if (steps[1]) {
+                setNextStep(steps[1])
+            }
+        }
+
+        return currentStep;
+    }
+
+    function handleTimer() {
+        if (!isCounting) {
+            console.log('start timer');
+            setIsCounting(true);
+            intervalRef.current = setInterval(() => {
+                setTimer(prevTimer => prevTimer + 1);
+            }, 1000);
+        } else {
+            console.log('pause timer');
+            setIsCounting(false);
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
+    }
+
+    function resetTimer() {
+        setIsCounting(false)
+        setTimer(0)
+        clearInterval(intervalRef.current);
+    }
+
+    function finishRound() {
+        resetTimer();
+
+        // There are next step, so its a new round
+        if (nextStep) {
+            steps.forEach((step, index) => {
+                if (steps[index].id === nextStep.id) {
+                    steps[index].in_progress = true;
+                } else {
+                    steps[index].in_progress = false;
+                }
+            })
+
+            setCurrentRound(currentRound + 1)
+            defineCurrentAndNextStep()
+
+        }else {
+            // There are no next step, si it a new cycle
+            setCurrentRound(1)
+            setCurrentCycle(currentCycle + 1)
+            steps[0].in_progress = true;
+            defineCurrentAndNextStep()
+        }
+
+
+    }
+
+    return (
+        <main className="flex min-h-screen flex-col items-center">
+            {!isLoading ? (
+                <>
+                    <div className={'h-[70vh] bg-green-400 w-full pt-2 cursor-pointer'} onClick={() => handleTimer()}>
+                        <Link className={'absolute font-bold text-xl left-2 top-2'} href={'/steps'}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                 className="size-8">
+                                <path fillRule="evenodd"
+                                      d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
+                                      clipRule="evenodd"/>
+                            </svg>
+                        </Link>
+                        {!isCounting ?
+                            <p className={'mx-auto w-full text-center h-0 font-light text-lg '}>Press the green part
+                                to start</p>
+                            : null}
+                        <div className={'w-28 right-0 top-2 absolute font-semibold text-lg'}>
+                            <div className={'grid grid-cols-3'}>
+                                <div className={'col-span-2'}>round :</div>
+                                <div>{currentRound}/{steps.length}</div>
+                            </div>
+                            <div className={'grid grid-cols-3'}>
+                                <div className={'col-span-2'}>cycle :</div>
+                                <div>{currentCycle}/{cycles_number}</div>
+                            </div>
+                        </div>
+
+                        <div className={' flex flex-col p-8 gap-8 justify-center h-full'}>
+                            <h1 className={'text-center text-6xl font-bold'}>{currentStep?.name}</h1>
+                            <p className={'text-center text-8xl font-bold'}>00:{currentStep?.duration - timer}</p>
+                        </div>
+                    </div>
+                    <div className={'h-[30vh] bg-blue-400 w-full flex flex-col p-8 gap-4 justify-center'}>
+                        {nextStep ?
+                            (
+                                <>
+                                    <h1 className={'text-center text-4xl font-bold'}>{nextStep?.name}</h1>
+                                    <p className={'text-center text-6xl font-bold'}>{'00:' + nextStep.duration}</p>
+                                </>
+                            ) : (
+                                <h1 className={'text-center text-6xl font-bold'}>{'Fini :)'}</h1>
+                            )
+                        }
+                    </div>
+                </>
+            ) : (
+                <div className={'flex flex-col h-screen justify-center'}>
+                    <h1 className={'text-center text-4xl font-bold'}>Loading...</h1>
+                </div>
+            )}
+        </main>
+    );
 }
